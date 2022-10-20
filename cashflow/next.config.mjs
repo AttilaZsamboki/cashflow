e11cb908b83d9dfad36541c9880e74cc674c9da0
AttilaZsamboki <99214950+AttilaZsamboki@users.noreply.github.com>
2022-10-20 21:48:49 +0200

    next bug fix

--- cashflow/next.config.mjs
+++ cashflow/next.config.mjs
@@ -15,7 +15,7 @@ function defineNextConfig(config) {
 
 export default defineNextConfig({
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: false,
   // Next.js i18n docs: https://nextjs.org/docs/advanced-features/i18n-routing
   i18n: {
     locales: ["en"],
