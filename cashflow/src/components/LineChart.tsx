3a450b98a101a83e0fbc35566378258ef7b91525
AttilaZsamboki <99214950+AttilaZsamboki@users.noreply.github.com>
2022-10-24 14:13:24 +0200

    var -> const

--- cashflow/src/components/LineChart.tsx
+++ cashflow/src/components/LineChart.tsx
@@ -3,7 +3,7 @@ import { Line } from "react-chartjs-2";
 
 const getDaysArray = function (start: Date, end: Date) {
   for (
-    var arr = [], dt = new Date(start);
+    const arr = [], dt = new Date(start);
     dt <= new Date(end);
     dt.setDate(dt.getDate() + 1)
   ) {
